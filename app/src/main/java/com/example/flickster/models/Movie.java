dd23be8cd689a83bd5b9a5087b9325adab3561c8
zarsla <you@example.com>
2020-02-12 16:36:02 -0500

    Initial commit

---
+++ app/src/main/java/com/example/flickster/models/Movie.java
@@ -0,0 +1,30 @@
+package com.example.flickster.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+    String posterPath;
+    String title;
+    String overView;
+
+    public Movie(JSONObject jsonObject) throws JSONException {
+        posterPath = jsonObject.getString("poster_path");
+        title = jsonObject.getString("title");
+        overView = jsonObject.getString("overview");
+
+    }
+
+    public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
+        List<Movie> movies = new ArrayList<>();
+        for(int i = 0; i < movieJsonArray.length(); i++){
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
+        }
+        return movies;
+
+    };
+}
